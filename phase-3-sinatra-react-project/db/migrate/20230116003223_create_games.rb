9a7980571ab66caed9b5f26f880c757dacfd2e28
Bobby De Luna <bobby.deluna2@gmail.com>
2023-01-15 17:41:59 -0700

    updated the schema

--- phase-3-sinatra-react-project/db/migrate/20230116003223_create_games.rb
+++ phase-3-sinatra-react-project/db/migrate/20230116003223_create_games.rb
@@ -1,4 +1,12 @@
 class CreateGames < ActiveRecord::Migration[6.1]
   def change
+
+    create_table :games do |t|
+      t.string :title
+      t.integer :genre_id
+      t.integer :platform_id
+      t.integer :year
+    end
+
   end
 end
--- phase-3-sinatra-react-project/db/migrate/20230116003418_create_platforms.rb
+++ phase-3-sinatra-react-project/db/migrate/20230116003418_create_platforms.rb
@@ -1,4 +1,9 @@
 class CreatePlatforms < ActiveRecord::Migration[6.1]
   def change
+
+    create_table :platforms do |t|
+      t.string :name
+    end
+
   end
 end
