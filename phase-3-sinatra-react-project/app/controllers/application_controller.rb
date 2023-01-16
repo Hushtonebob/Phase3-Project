f3a0b6c25ea828714dae2525c6b0d49e486d66df
Bobby De Luna <bobby.deluna2@gmail.com>
2023-01-15 21:22:20 -0700

    Created Route for an indivdual game

--- phase-3-sinatra-react-project/app/controllers/application_controller.rb
+++ phase-3-sinatra-react-project/app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/games" do
+    Game.all.to_json
   end
 
+  get '/games/:id' do 
+    game = Game.find(params[:id])
+    game.to_json
+  end
+  
 end
